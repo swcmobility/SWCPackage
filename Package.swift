017c61d5ad77107beb80d2a29d4d1f580e323406
Yasheed <yasheedv@gmail.com>
2023-03-06 11:16:03 +0400

    framework change

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SWCPackage",
-            targets: ["SWCFramework"]),
+            targets: ["SWCPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "SWCFramework", path: "./Sources/SWCFramework.xcframework")
+        .binaryTarget(name: "SWCPackage", path: "./Sources/SWCPackage.xcframework")
     ]
 )
